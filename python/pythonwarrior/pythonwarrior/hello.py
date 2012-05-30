8fdec178a6a0b5adc7dbfa2e07d7948ae53dc6f8
daniel glenn <linuxbender@gmail.com>
2012-05-30 03:02:08 +0200

    add pythonwarrior django 1.4

---
+++ python/pythonwarrior/pythonwarrior/hello.py
@@ -0,0 +1,9 @@
+import django
+
+def application(environ, start_response):
+    status = '200 OK'
+    output = django.get_version()
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+    return [output]
